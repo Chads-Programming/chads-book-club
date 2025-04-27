89935672ccca7833a056c0e02bd00845fa2afabe
MarcialFantini <46429781+MarcialFantini@users.noreply.github.com>
2025-04-26 22:02:34 -0300

    Feat  card book (#1)
    * feat: card book added
    
    * fix: component stars rating

---
+++ src/components/RenderStars/index.tsx
@@ -0,0 +1,5 @@
+import { CiStar } from "react-icons/ci";
+
+export function RenderStars({ count }: { count: number }) {
+  return Array.from({ length: count }, (_, i) => <CiStar key={i} size={20} />);
+}
