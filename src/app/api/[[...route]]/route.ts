89d722337d7d5a67102047b183d0eb0e44283932
Krb0 <maurodavidcaceres@gmail.com>
2025-04-27 00:57:50 +0000

    feat: add missing http methods

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -3,3 +3,7 @@ import { app } from "@/api/main"
 
 export const GET = handle(app)
 export const POST = handle(app)
+export const DELETE = handle(app)
+export const PATCH = handle(app)
+export const PUT = handle(app)
+
