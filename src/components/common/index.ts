34714d19cb8ee83fe9527602b3127555682e702f
FOWMind <alwl1@hotmail.com>
2025-04-29 01:02:57 -0300

    feat: components common folder barrel

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,1 +1,4 @@
 export { Heading, headingVariants } from "./heading"
+export { Paragraph, paragraphVariants } from "./paragraph"
+export { CardBook } from "./card-book"
+export { Rating } from "./rating"
